5203158223ae282d822a41058d575806699d61a7
Ileriayo Adebiyi <ileriayoadebiyi@gmail.com>
2020-06-15 15:01:41 +0100

    define inference utility functions

--- inference.py
+++ inference.py
@@ -35,33 +35,62 @@ class Network:
     """
 
     def __init__(self):
-        ### TODO: Initialize any class variables desired ###
+        self.ie_plugin = None
+        self.net = None
+        self.input_blob = None
+        self.output_blob = None
+        self.exec_network = None
 
-    def load_model(self):
-        ### TODO: Load the model ###
-        ### TODO: Check for supported layers ###
-        ### TODO: Add any necessary extensions ###
-        ### TODO: Return the loaded inference plugin ###
-        ### Note: You may need to update the function parameters. ###
-        return
+    def load_model(self, model_xml, device, cpu_extension):
+        # Create IECore
+        log.info('Creating IECore...')
+        self.ie_plugin = IECore()
+
+        # Add CPU extension to IECore
+        if cpu_extension and 'CPU' in device:
+            log.info('Adding CPU extension:\n\t{}'.format(cpu_extension))
+            self.ie_plugin.add_extension(cpu_extension, device)
+
+        # Load the IR model into IENetwork
+        model_bin = os.path.splitext(model_xml)[0] + '.bin'
+        log.info("Loading model into IENetwork:\n\t{}\n\t{}".format(model_xml, model_bin))
+        
+        # self.net = IENetwork(model=model_xml, weights=model_bin) # Reading network using constructor is deprecated
+        self.net = self.ie_plugin.read_network(model=model_xml, weights=model_bin)
+
+        # Check layers
+        log.info('Current device specified: {}'.format(device))
+        log.info("Checking for unsupported layers...")
+        supported_layers = self.ie_plugin.query_network(network=self.net, device_name='CPU')
+        unsupported_layers = [l for l in self.net.layers.keys() if l not in supported_layers]
+        if len(unsupported_layers) != 0:
+            log.error('These layers are unsupported:\n{}'.format(', '.join(unsupported_layers)))
+            log.error('Specify an available extension to add to IECore from the command line using -l/--cpu_extension')
+            exit(1)
+        else:
+            log.info('All layers are supported!')
+
+        # Load the model network into IECore
+        self.exec_network = self.ie_plugin.load_network(self.net, device)
+        log.info("IR Model has been successfully loaded into IECore")
+
+        self.input_blob = next(iter(self.net.inputs))
+        self.output_blob = next(iter(self.net.outputs))
+        return self.exec_network
 
     def get_input_shape(self):
-        ### TODO: Return the shape of the input layer ###
-        return
+        return self.net.inputs[self.input_blob].shape
 
-    def exec_net(self):
-        ### TODO: Start an asynchronous request ###
-        ### TODO: Return any necessary information ###
-        ### Note: You may need to update the function parameters. ###
-        return
+    def exec_net(self, image):
+        # Async request
+        self.infer_request_handle = self.exec_network.start_async(0, { self.input_blob: image })
+        return self.infer_request_handle
 
     def wait(self):
-        ### TODO: Wait for the request to be complete. ###
-        ### TODO: Return any necessary information ###
-        ### Note: You may need to update the function parameters. ###
-        return
+        # Wait for completion of request
+        status = self.exec_network.requests[0].wait(-1)
+        return status
 
     def get_output(self):
-        ### TODO: Extract and return the output results
-        ### Note: You may need to update the function parameters. ###
-        return
+        # Extract and return the output results
+        return self.exec_network.requests[0].outputs[self.output_blob]
